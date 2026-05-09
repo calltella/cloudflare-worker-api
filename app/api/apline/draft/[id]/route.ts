cc0a5397665cfd9196a5aca6178ea76dcca62a16
calltella <yuichi.asa@gmail.com>
2026-05-09 08:04:19 +0000

    draft id

--- app/api/apline/draft/[id]/route.ts
+++ app/api/apline/draft/[id]/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { getAplineDraftsExists } from "@/src/service/aplineSub.service";
 
 export async function DELETE(
   request: Request,
@@ -11,5 +12,20 @@ export async function DELETE(
   return NextResponse.json({
     success: true,
     id,
+  });
+}
+
+export async function GET(
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  console.log(`draft get id: ${id}`);
+
+  const res = await getAplineDraftsExists(Number(id))
+
+  return NextResponse.json({
+    success: true,
+    res
   });
 }
