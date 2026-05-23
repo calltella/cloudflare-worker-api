4fc1f1541178db1cd7ed391a2c7b714d422a428d
calltella <yuichi.asa@gmail.com>
2026-05-23 01:26:03 +0000

    email isexststs

--- app/api/user/search/route.ts
+++ app/api/user/search/route.ts
@@ -2,17 +2,39 @@
 
 import { requireAuth } from "@/lib/utils/auth";
 import { NextRequest, NextResponse } from "next/server";
+import { findUserByEmail } from "@/src/service/user.service";
 
-/**
- * email にユーザーがぞんざいするか
- * @param req 
- * @returns 
- */
-export async function POST(request: NextRequest) {
-  const auth = await requireAuth(request);
-  if (!auth.ok) return auth.response;
+// 認証なしでemailがユーザー登録されているか返す
+export async function GET(
+  request: NextRequest,
+) {
 
-  console.log(`req: ${JSON.stringify(request)}`)
+  const { searchParams } = request.nextUrl;
 
-  return NextResponse.json(auth)
+  const requestEmail = searchParams.get("email");
+  if (!requestEmail) {
+    return NextResponse.json(
+      { success: false, message: "emailが指定されていません" },
+      { status: 404 }
+    );
+  }
+
+  try {
+    const res = await findUserByEmail(requestEmail);
+
+    if (!res) {
+      return NextResponse.json(
+        { success: true, data: { isExits: false } }
+      );
+    }
+    return NextResponse.json({
+      success: true,
+      data: { isExits: true }
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: "エラーが発生しました" },
+      { status: 404 }
+    );
+  }
 }
